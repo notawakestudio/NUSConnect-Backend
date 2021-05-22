d7c6863f4b8efc8138b53bfde02e54d15bf76128
tlylt <liu_yongliang@hotmail.com>
2021-05-22 19:17:08 +0800

    minor fixes

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ from flask_cors import CORS
 
 deta = Deta()
 
-db = deta.Base("nusconnect")
+db_question = deta.Base("nusconnect")
+db_quiz = deta.Base("nusconnect-quiz")
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=["GET"])
-def getData():
-    return jsonify(next(db.fetch()))
+@app.route('/quiz/question/<questionId>', methods=["GET"])
+def getQuestionById(questionId):
+    return jsonify(db_question.get(questionId))
 
-@app.route('/', methods=["POST"])
-def postData():
-    data = request.get_json()
+@app.route('/quiz/question', methods=["GET"])
+def getAllQuestion():
+    return jsonify(next(db_question.fetch()))
+
+@app.route('/quiz/make', methods=["POST"])
+def postQuestion():
+    data = request.get_json(force=True)
     if data is None:
         return jsonify({"error":"Not in JSON format"})
     if 'id' not in data:
@@ -30,5 +35,41 @@ def postData():
         return jsonify({"error":"No correct answers"})
     if 'incorrect_answers' not in data:
         return jsonify({"error":"No incorrect answers"})
-    result = db.put(request.json)
+    # use id received as key in deta base
+    result = db_question.put(request.json, data['id'])
     return jsonify(result, 201)
+
+@app.route('/quiz/quiz/<quizId>', methods=["GET"])
+def getQuizById(quizId):
+    return jsonify(db_quiz.get(quizId))
+
+@app.route('/quiz/quiz', methods=["GET"])
+def getAllQuiz():
+    return jsonify(next(db_quiz.fetch()))
+
+
+@app.route('/quiz/collate', methods=["POST"])
+def postQuiz():
+    data = request.get_json(force=True)
+    if data is None:
+        return jsonify({"error":"Not in JSON format"})
+    if 'id' not in data:
+        return jsonify({"error":"No ID"})
+    if 'modules' not in data:
+        return jsonify({"error":"No modules"})
+    if 'title' not in data:
+        return jsonify({"error":"No title"})
+    if 'questions' not in data:
+        return jsonify({"error":"No questions"})
+    if 'tags' not in data:
+        return jsonify({"error":"No tags"})
+    if 'week' not in data:
+        return jsonify({"error":"No week"})
+    if 'author' not in data:
+        return jsonify({"error":"No author"})
+    if 'date' not in data:
+        return jsonify({"error":"No date"}) 
+    # use id received as key in deta base
+    result = db_quiz.put(request.json, data['id'])
+    return jsonify(result, 201)
+
